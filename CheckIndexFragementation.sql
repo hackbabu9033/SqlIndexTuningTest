ce9700ebef8e1a4b8b1bf1ea44281451d48ff0a8
hackbabu <hack870629@gmail.com>
2022-02-27 18:44:03 +0800

    檢視這三種table的碎片索引狀況

---
+++ CheckIndexFragementation.sql
@@ -0,0 +1,27 @@
+﻿
+select  * from sys.dm_db_index_physical_stats
+(
+ DB_ID('IndexTuningTest'),
+ OBJECT_ID('dbo.Accounts'),
+ 0,
+ null,
+ null
+);
+
+select  * from sys.dm_db_index_physical_stats
+(
+ DB_ID('IndexTuningTest'),
+ OBJECT_ID('dbo.AccountWithCluster'),
+ 1,
+ null,
+ null
+);
+
+select  * from sys.dm_db_index_physical_stats
+(
+ DB_ID('IndexTuningTest'),
+ OBJECT_ID('dbo.AccountWithClusterSeqField'),
+ 1,
+ null,
+ null
+);
